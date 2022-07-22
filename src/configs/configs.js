cd3ac89367bee86a9d9371b8ee4b5b1877d4d57a
felipealmeida13 <f.almeida08@hotmail.com>
2022-07-21 23:35:50 -0300

    first commit

---
+++ src/configs/configs.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Configs {
+    constructor() {
+        this.host = "smtp.ethereal.email";
+        this.port = 587;
+        this.user = "demario.williamson38@ethereal.email";
+        this.password = "bXSMCjxnbvKvZHzvdn";
+    }
+}
+exports.default = new Configs;
---
+++ src/configs/configs.ts
@@ -0,0 +1,11 @@
+
+
+class Configs {
+    public host = "smtp.ethereal.email";
+    public port = 587;
+    public user = "demario.williamson38@ethereal.email";
+    public password = "bXSMCjxnbvKvZHzvdn";
+
+}
+
+export default new Configs;
